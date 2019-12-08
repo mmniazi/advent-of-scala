5e608253982c01e60d82d49b0fda589fb914d6b4
Mohsin Niazi <mohsin@apple.com>
2019-12-08 23:06:57 +0000

    concise pattern matching

--- src/main/scala/day5/Day5.scala
+++ src/main/scala/day5/Day5.scala
@@ -4,7 +4,11 @@ import scala.annotation.tailrec
 import scala.io.Source
 
 object Day5 {
-  object Op { def apply(code: Int): Op = new Op(code, code % 100) }
+
+  object Op {
+    def apply(code: Int): Op = new Op(code, code % 100)
+  }
+
   case class Op(code: Int, opType: Int) {
     val (p1m, p2m, p3m) = (code % 1000 / 100, code % 10000 / 1000, code / 10000)
     def p1(implicit intCode: Array[Int], pointer: Int): Int = if (p1m == 1) pointer + 1 else intCode(pointer + 1)
@@ -13,47 +17,39 @@ object Day5 {
   }
 
   @tailrec
-  def compute(implicit intCode: Array[Int], input: List[Int], output: List[Int] = Nil, pointer: Int = 0): List[Int] =
-    intCode
-      .lift(pointer)
-      .map(Op(_))
-    match {
-      case Some(op) if op.opType == 1 =>
+  def compute(implicit intCode: Array[Int], input: List[Int], output: List[Int] = Nil, pointer: Int = 0): List[Int] = Op(intCode(pointer)) match {
+      case op@Op(_, 1) =>
         intCode(op.p3) = intCode(op.p1) + intCode(op.p2)
         compute(intCode, input, output, pointer + 4)
 
-      case Some(op) if op.opType == 2 =>
+      case op@Op(_, 2) =>
         intCode(op.p3) = intCode(op.p1) * intCode(op.p2)
         compute(intCode, input, output, pointer + 4)
 
-      case Some(op) if op.opType == 3 =>
+      case op@Op(_, 3) =>
         intCode(op.p1) = input.head
         compute(intCode, input.tail, output, pointer + 2)
 
-      case Some(op) if op.opType == 4 =>
+      case op@Op(_, 4) =>
         compute(intCode, input, output :+ intCode(op.p1), pointer + 2)
 
-      case Some(op) if op.opType == 5 =>
+      case op@Op(_, 5) =>
         if (intCode(op.p1) != 0) compute(intCode, input, output, intCode(op.p2))
         else compute(intCode, input, output, pointer + 3)
 
-      case Some(op) if op.opType == 6 =>
+      case op@Op(_, 6) =>
         if (intCode(op.p1) == 0) compute(intCode, input, output, intCode(op.p2))
         else compute(intCode, input, output, pointer + 3)
 
-      case Some(op) if op.opType == 7 =>
+      case op@Op(_, 7) =>
         intCode(op.p3) = if (intCode(op.p1) < intCode(op.p2)) 1 else 0
         compute(intCode, input, output, pointer + 4)
 
-      case Some(op) if op.opType == 8 =>
+      case op@Op(_, 8) =>
         intCode(op.p3) = if (intCode(op.p1) == intCode(op.p2)) 1 else 0
         compute(intCode, input, output, pointer + 4)
 
-      case Some(op) if op.opType == 99 => output
-
-      case Some(_) => throw new Exception()
-
-      case None => output
+      case _ => output
     }
 
   def main(args: Array[String]): Unit = {
