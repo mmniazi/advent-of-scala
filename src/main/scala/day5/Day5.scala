bfc270c8efeef26434fa989e100ac9d88295d464
Mohsin Niazi <mohsin@apple.com>
2019-12-08 23:25:08 +0000

    add update method to make code more concise

--- src/main/scala/day5/Day5.scala
+++ src/main/scala/day5/Day5.scala
@@ -8,45 +8,50 @@ object Day5 {
   def compute(implicit intCode: Array[Int], input: List[Int], output: List[Int] = Nil, pointer: Int = 0): List[Int] =
     Op(intCode(pointer)) match {
       case op@Op(_, 1) =>
-        intCode(op.p3) = intCode(op.p1) + intCode(op.p2)
+        op.p3 = op.p1 + op.p2
         compute(intCode, input, output, pointer + 4)
 
       case op@Op(_, 2) =>
-        intCode(op.p3) = intCode(op.p1) * intCode(op.p2)
+        op.p3 = op.p1 * op.p2
         compute(intCode, input, output, pointer + 4)
 
       case op@Op(_, 3) =>
-        intCode(op.p1) = input.head
+        op.p1 = input.head
         compute(intCode, input.tail, output, pointer + 2)
 
       case op@Op(_, 4) =>
-        compute(intCode, input, output :+ intCode(op.p1), pointer + 2)
+        compute(intCode, input, output :+ op.p1, pointer + 2)
 
       case op@Op(_, 5) =>
-        if (intCode(op.p1) != 0) compute(intCode, input, output, intCode(op.p2))
+        if (op.p1 != 0) compute(intCode, input, output, op.p2)
         else compute(intCode, input, output, pointer + 3)
 
       case op@Op(_, 6) =>
-        if (intCode(op.p1) == 0) compute(intCode, input, output, intCode(op.p2))
+        if (op.p1 == 0) compute(intCode, input, output, op.p2)
         else compute(intCode, input, output, pointer + 3)
 
       case op@Op(_, 7) =>
-        intCode(op.p3) = if (intCode(op.p1) < intCode(op.p2)) 1 else 0
+        op.p3 = if (op.p1 < op.p2) 1 else 0
         compute(intCode, input, output, pointer + 4)
 
       case op@Op(_, 8) =>
-        intCode(op.p3) = if (intCode(op.p1) == intCode(op.p2)) 1 else 0
+        op.p3 = if (op.p1 == op.p2) 1 else 0
         compute(intCode, input, output, pointer + 4)
 
       case _ => output
     }
 
-  object Op { def apply(code: Int): Op = new Op(code, code % 100) }
+  object Op {
+    def apply(code: Int): Op = new Op(code, code % 100)
+  }
+
   case class Op(code: Int, opType: Int) {
-    val (p1m, p2m, p3m) = (code % 1000 / 100, code % 10000 / 1000, code / 10000)
-    def p1(implicit intCode: Array[Int], pointer: Int): Int = if (p1m == 1) pointer + 1 else intCode(pointer + 1)
-    def p2(implicit intCode: Array[Int], pointer: Int): Int = if (p2m == 1) pointer + 2 else intCode(pointer + 2)
-    def p3(implicit intCode: Array[Int], pointer: Int): Int = if (p3m == 1) pointer + 3 else intCode(pointer + 3)
+    val (p1mode, p2mode, p3mode) = (code % 1000 / 100, code % 10000 / 1000, code / 10000)
+    def p1(implicit intCode: Array[Int], p: Int): Int = intCode(if (p1mode == 1) p + 1 else intCode(p + 1))
+    def p2(implicit intCode: Array[Int], p: Int): Int = intCode(if (p2mode == 1) p + 2 else intCode(p + 2))
+    def p3(implicit intCode: Array[Int], p: Int): Int = intCode(if (p3mode == 1) p + 3 else intCode(p + 3))
+    def p1_=(value: Int)(implicit intCode: Array[Int], p: Int): Unit = intCode(if (p1mode == 1) p + 1 else intCode(p + 1)) = value
+    def p3_=(value: Int)(implicit intCode: Array[Int], p: Int): Unit = intCode(if (p3mode == 1) p + 3 else intCode(p + 3)) = value
   }
 
   def main(args: Array[String]): Unit = {
