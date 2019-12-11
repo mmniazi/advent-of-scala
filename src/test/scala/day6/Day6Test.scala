453c9bf00959d461db62b95a1c484f0684e4bffd
Mohsin Niazi <mohsin@apple.com>
2019-12-11 08:00:16 +0000

    day 6 solution

---
+++ src/test/scala/day6/Day6Test.scala
@@ -0,0 +1,28 @@
+package day6
+
+import day6.Day6._
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day6Test extends AnyFunSuite {
+
+  test("test createTree") {
+    val nodes = List(("COM", "B"), ("B", "C"), ("C", "D"), ("B", "G"), ("G", "H"))
+    val result = createTree(nodes).toString
+    val expected = "COM(B(C(D()),G(H())))"
+    assert(result == expected)
+  }
+
+  test("test calculateOrbits") {
+    val nodes = List(("COM", "B"), ("B", "C"), ("C", "D"), ("D", "E"), ("E", "F"), ("B", "G"),
+      ("G", "H"), ("D", "I"), ("E", "J"), ("J", "K"), ("K", "L"))
+    val (tree, map) = createTree(nodes)
+    assert(calculateOrbits(tree) == 42)
+  }
+
+  test("test calculateDistance") {
+    val nodes = List(("COM","B"), ("B","C"), ("C","D"), ("D","E"), ("E","F"), ("B","G"), ("G","H"),
+      ("D","I"), ("E","J"), ("J","K"), ("K","L"), ("K","YOU"), ("I","SAN"))
+    val (_, map) = createTree(nodes)
+    assert(calculateDistance(Some(map("YOU")), Some(map("SAN"))) == 4)
+  }
+}
