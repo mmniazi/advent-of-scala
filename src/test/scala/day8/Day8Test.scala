ebb789abf30fcf9a04228b0dc2e9c5cf390e630f
Mohsin Niazi <mohsin@apple.com>
2019-12-14 18:15:57 +0000

    day 8 solution

---
+++ src/test/scala/day8/Day8Test.scala
@@ -0,0 +1,16 @@
+package day8
+
+import day8.Day8._
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day8Test extends AnyFunSuite {
+  test("test layerWithMinZeroes") {
+    val input = List(1, 2, 2, 1, 0, 1, 1, 2, 0, 0, 0, 0)
+    assert(calculateHash(input, 3 * 2) == 6)
+  }
+
+  test("test decodeImage") {
+    val input = List(0, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 2, 0, 0, 0, 0)
+    assert(decodeImage(input, 2 * 2) == List(0,1,1,0))
+  }
+}
