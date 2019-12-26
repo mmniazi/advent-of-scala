ee23f3c6043b653c6104d3d294a4efff4b71d4dc
Mohsin Niazi <mohsin@apple.com>
2019-12-26 14:10:38 +0000

    day 18 bootstrap

---
+++ src/main/scala/day18/Day18.scala
@@ -0,0 +1,12 @@
+package day18
+
+import scala.io.Source
+
+object Day18 {
+
+  def main(args: Array[String]): Unit = {
+    val input = Source.fromResource("day18.txt").getLines().toList
+
+
+  }
+}
---
+++ src/test/scala/day18/Day18Test.scala
@@ -0,0 +1,14 @@
+package day18
+
+import org.scalatest.funsuite.AnyFunSuite
+
+
+class Day18Test extends AnyFunSuite {
+  test("test") {
+    val input =
+      """#########
+        |#b.A.@.a#
+        |#########""".stripMargin
+
+  }
+}
